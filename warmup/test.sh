66eb5891046d223c62e679cad7f546a48b0a62da
Ethan Jennings <ethanhjennings@gmail.com>
2016-05-19 03:31:33 -0400

    Fixing typo

--- warmup/test.sh
+++ warmup/test.sh
@@ -1,8 +1,8 @@
-python3 warmup_solution.py simple_dict.json < tests/test_simple.in > tests/test_simple.out
+python3 warmup.py simple_dict.json < tests/test_simple.in > tests/test_simple.out
 
 if ! cmp "tests/test_simple.out" "tests/test_simple.expected"
 then
-    echo "test_simple.out doesn't match test_simple.expected!"
+    echo "'test_simple.out' doesn't match 'test_simple.expected'!"
     echo "Failed test #1: test_simple"
     exit
 else
@@ -10,11 +10,11 @@ else
 fi
 
 
-python3 warmup_solution.py websters_dict.json < tests/test_websters.in > tests/test_websters.out
+python3 warmup.py websters_dict.json < tests/test_websters.in > tests/test_websters.out
 
 if ! cmp "tests/test_websters.out" "tests/test_websters.expected"
 then
-    echo "test_websters.out doesn't match test_websters.expected!"
+    echo "'test_websters.out' doesn't match 'test_websters.expected'!"
     echo "Failed test #2: test_websters"
     exit
 else
